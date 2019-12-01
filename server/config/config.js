24edff52fa4622339ac57df104b22b4b493ca6fc
seba traverso <sebastiantraverso@gmail.com>
2019-12-01 11:39:45 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://sebaBase:zdPVDCaWMMQ75kjb@cluster0-tdaoq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
